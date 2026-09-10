65e3f9ff0a75a1621f0a6afe3868d1ed9bfb8708
Nivash <nivashsingh@gmail.com>
2026-09-10 23:44:55 +0200

    config: allow QA RGS host for deployed games

--- apps/boulder-bash/vite.config.js
+++ apps/boulder-bash/vite.config.js
@@ -1,4 +1,4 @@
 // @ts-ignore
 import config from 'config-vite';
 // envPrefix exposes PUBLIC_* (PUBLIC_RGS_URL) via import.meta.env.
-export default { ...config(), envPrefix: ['VITE_', 'PUBLIC_'], server: { allowedHosts: ['thor.tail8ad778.ts.net'] } };
+export default { ...config(), envPrefix: ['VITE_', 'PUBLIC_'], server: { allowedHosts: ['uat-rgs.atomic-labs.co'] } };
